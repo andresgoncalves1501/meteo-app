f1c90fe460c74a1db1b4cc479cdabc9854543aed
André Gonçalves <andresg1501@gmail.com>
2020-09-10 15:00:20 +0100

    meteo-app commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static("express"));
+app.use(express.static(path.join(__dirname, '/public')));
+
+// default URL for website
+app.use('/', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+const server = http.createServer(app);
+const port = 3000;
+
+server.listen(port);
+
+console.debug('Server listening on port ' + port);
